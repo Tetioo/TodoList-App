85bf1d2de492ed1eeccf91f3514deb32eed5fdef
Rotich <104131298+Tetioo@users.noreply.github.com>
2023-12-31 18:43:29 +0300

    new file

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,8 @@
 import Form from "./components/Form";
 import Header from "./components/Header"; 
 import { useState } from "react";
+import Todo from "./components/Todo";
+
 function App() { //to be accessed by any file
   const [todo,setTodo] =useState(' ')
   const[todolist,setTodoList]= useState([])
@@ -13,6 +15,7 @@ function App() { //to be accessed by any file
       todolist={todolist}
       setTodoList={setTodoList}>
       </Form>
+      <Todo></Todo>
     </div>
   );
 }
--- todo/src/components/Form.js
+++ todo/src/components/Form.js
@@ -2,11 +2,20 @@ import styles from "../style.module.css"
 
 
 const Form=({todo, setTodo,todolist,setTodoList}) => { //accept props
+    const handleChange=(event)=>{
+        setTodo(event.target.value) //capture value inside todo and add it to the todolist when submitting
+        console.log(todo)
+    }
+    const handleSubmit=(event)=>{
+        event.preventDefault()
+        setTodoList([[...todolist,todo]]) //get initial items first and add to the array then add new items(todo)
+        console.log(todolist)
+    }
     return (
         <div className={styles.todoform}>
-            <form>
-                <input className={styles.todoinput} placeholder="Add Todo Item"></input>
-                <button className={styles.todobutton}> ADD</button>
+            <form onSubmit={handleSubmit}>
+                <input value ={todo} onChange={handleChange} className={styles.todoinput} placeholder="Add Todo Item"></input>
+                <button type="submit" className={styles.todobutton}> ADD</button>
             </form>
         </div>
     )
